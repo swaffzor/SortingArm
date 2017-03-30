8fad9f637175aabaacf899fb593d5a5afb0ade74
swaffzor <jeremy@swafford.co>
2017-03-30 11:12:09 -0400

    initial empty

---
+++ SortingArm/SortingArm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SortingArm
+//
+//  Created by Jeremy Swafford on 3/30/17.
+//  Copyright © 2017 NCSU ECE Project 14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
